8c0caac516a7e32c9bec85569d38d58b0d08bfac
Ang Kah Min, Kelvin <syraxius@hotmail.com>
2021-08-01 10:00:30 +0800

    Dynamically obtain base address of process

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import win32con
 import win32gui
 
 import ctypes
+import pywintypes
 import win32process
 from ctypes import wintypes
 
@@ -25,14 +26,15 @@ def get_hwnd(name, winlist):
     return [(hwnd, title) for hwnd, title in winlist if name in title][0][0]
 
 
-def keyboard_send_vk_as_scan_code(hwnd, vk, extended=0):
-    scan_code = win32api.MapVirtualKey(vk, 0)
-    lparam = extended << 24 | scan_code << 16
-    win32gui.SendMessage(hwnd, win32con.WM_KEYDOWN, vk, lparam)
-    win32gui.SendMessage(hwnd, win32con.WM_KEYUP, vk, lparam)
+def get_hwnd_base_address(hwnd):
+    pid = win32process.GetWindowThreadProcessId(hwnd)[1]
+    handle = win32api.OpenProcess(win32con.PROCESS_QUERY_INFORMATION | win32con.PROCESS_VM_READ, pywintypes.FALSE, pid)
+    base_address = win32process.EnumProcessModules(handle)[0]
+    win32api.CloseHandle(handle)
+    return base_address
 
 
-def get_memory_values(hwnd, address_description):
+def get_memory_values(hwnd, base_address, address_description):
     # Specify the params and return type of ReadProcessMemory
     # https://stackoverflow.com/questions/33855690/trouble-with-readprocessmemory-in-python-to-read-64bit-process-memory
     ctypes.windll.kernel32.ReadProcessMemory.argtypes = [wintypes.HANDLE, wintypes.LPCVOID, wintypes.LPVOID, ctypes.c_size_t, ctypes.POINTER(ctypes.c_size_t)]
@@ -45,13 +47,13 @@ def get_memory_values(hwnd, address_description):
     address_buffer = ctypes.c_uint64()
     final_buffer = ctypes.c_uint32()
 
-    base_pointer = address_description['base']
+    base_pointer = base_address + address_description['base_address_offset']
     ctypes.windll.kernel32.ReadProcessMemory(handle, base_pointer, ctypes.byref(address_buffer), ctypes.sizeof(address_buffer), None)
     next_address = address_buffer.value
     position = 1
-    for offset in address_description['offsets']:
-        next_address = next_address + offset
-        if position != len(address_description['offsets']):
+    for pointer_offset in address_description['pointer_offsets']:
+        next_address = next_address + pointer_offset
+        if position != len(address_description['pointer_offsets']):
             ctypes.windll.kernel32.ReadProcessMemory(handle, next_address, ctypes.byref(address_buffer), ctypes.sizeof(address_buffer), None)
             next_address = address_buffer.value
     ctypes.windll.kernel32.ReadProcessMemory(handle, next_address, ctypes.byref(final_buffer), ctypes.sizeof(final_buffer), None)
@@ -61,10 +63,17 @@ def get_memory_values(hwnd, address_description):
     return curr_value
 
 
+def keyboard_send_vk_as_scan_code(hwnd, vk, extended=0):
+    scan_code = win32api.MapVirtualKey(vk, 0)
+    lparam = extended << 24 | scan_code << 16
+    win32gui.SendMessage(hwnd, win32con.WM_KEYDOWN, vk, lparam)
+    win32gui.SendMessage(hwnd, win32con.WM_KEYUP, vk, lparam)
+
+
 address_descriptions = {
     'mp': {
-        'base': 0x7ff76d178210,
-        'offsets': (0x1CC,)
+        'base_address_offset': 0x01DB8210,
+        'pointer_offsets': (0x1CC,)
     },
 }
 
@@ -92,6 +101,7 @@ def cast(hwnd, spell):
 def main():
     winlist = get_winlist()
     hwnd = get_hwnd('FINAL FANTASY XIV', winlist)
+    base_address = get_hwnd_base_address(hwnd)
 
     class BlackMageState(enum.Enum):
         ICE = 1
@@ -100,10 +110,10 @@ def main():
     state = BlackMageState.FIRE
     transpose_timestamp = time.time()
     while True:
-        mp = get_memory_values(hwnd, address_descriptions['mp'])
+        mp = get_memory_values(hwnd, base_address, address_descriptions['mp'])
         transpose_delta = time.time() - transpose_timestamp
 
-        keyboard_send_vk_as_scan_code(hwnd, win32con.VK_TAB)  # Hotkey for targeting nearest enemy
+        keyboard_send_vk_as_scan_code(hwnd, win32con.VK_F11)  # Hotkey for targeting nearest enemy
 
         if state == BlackMageState.FIRE:
             if mp < 2000:
